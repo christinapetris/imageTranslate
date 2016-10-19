d10a59ee8fe8e8dfb31173d87afd8fc5502057b0
christinapetris <christina.petris@outlook.com>
2016-10-19 23:13:52 +1300

    4000?

--- app.js
+++ app.js
@@ -69,8 +69,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!');
 });
 
 module.exports = app;
